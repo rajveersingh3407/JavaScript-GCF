a4bea6eebb31a1ed666439ac6c900e304363ff9b
Rajveer Singh <rajveersingh17400@gmail.com>
2026-04-14 22:45:16 -0700

    Add files via upload

---
+++ java script/notes.md
@@ -0,0 +1,16 @@
+https://aman-0402.github.io/JAVASCRIPT/content.html 
+
+-> Javascript - It is a scripting language, used in Frontend
+-> Java - it is a programming language, used is backend
+
+**datatypes**
+
+1. Primitive -> It can store immutable values. 
+                examples -> string, number, boolean, null, undifined, symbol, bigint.
+
+2. Non-Primitive -> Reference values stored by address in memory.
+                examples -> object, array, function.
+
+-> The default datatype of input field in html forms is text(string) 
+-> console.log(Boolean("")); ->this will give false
+-> console.log(Boolean("Hi")); -> this will give true
---
+++ java script/script.js
@@ -0,0 +1,72 @@
+// var name ="Rajveer Singh";
+// let age = 18;
+// const country = "India";
+// console.log("Name: " + name);
+// console.log("Age: " + age);
+// console.log("Country: " + country);
+
+// const pi=3.14;
+// try{
+//     pi=3.14159; 
+// }catch(e){
+//     console.error("Error: " + e.message);
+// }
+
+// let name = "Rajveer Singh";
+// let age = 18;
+// let isStudent = true;
+// let emptyValue = null;
+// let notAssigned; //undefined
+// let uniqueId = Symbol("id");
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isStudent);
+// console.log(typeof emptyValue);
+// console.log(typeof notAssigned);
+// console.log(typeof uniqueId);
+
+// let fruits = ["mango","apple","banana"];
+// console.log(Array.isArray(fruits));
+// console.log(fruits);
+// console.log(fruits[0]);
+// let obj1={value:10};
+// let obj2=obj1; //both point to the same object
+// obj2.value=20;
+// console.log(obj1.value);
+
+//console.log(typeof null); // this is a object
+//console.log(typeof []); // this is a object
+
+// let result1="5"+2;
+// console.log(result1);
+// console.log(typeof result1);
+
+// let result2="5"-2;  //  - always forces numeric output
+// console.log(result2);
+// console.log(typeof result2);
+
+// try{
+//     let result=10/0;
+//     console.log(result);
+// }catch(error){
+//     console.log("Something went wrong:",error.message);
+// }finally{
+//     console.log("this always runs");
+// }
+
+// function checkage(age){
+//     try{
+//         if(age<18){
+//             throw new Error("you are underage");
+//         }
+//         console.log("access granted");
+//     }catch(error){
+//         console.log("blocked:",error.message);
+//     }
+// }
+// checkage(16);
+// checkage(21);
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
