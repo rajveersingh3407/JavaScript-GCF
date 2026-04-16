cc149c89ca369a344f3e6e66e6bf30774928069a
Rajveer Singh <rajveersingh17400@gmail.com>
2026-04-16 14:03:11 +0530

    js

--- java script/script.js
+++ java script/script.js
@@ -79,9 +79,10 @@
 
 // console.log(typeof window); // o/p -> object
 
-for (var i = 0; i < 3; i++) {
-    setTimeout(() => console.log(i), 1000);}
+// for (var i = 0; i < 3; i++) {
+//     setTimeout(() => console.log(i), 1000);}
 
-for (let i = 0; i < 3; i++) {
-    setTimeout(() => console.log(i), 1000);
-}
+// for (let i = 0; i < 3; i++) {
+//     setTimeout(() => console.log(i), 1000);
+// }
+
